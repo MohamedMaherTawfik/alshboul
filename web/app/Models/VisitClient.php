c16ee9af144dff98060436d85be4ea0efe13a58b
Mohamed Maher <m7mdellham77@gmail.com>
2025-07-27 18:40:06 +0300

    add the rest of the database

--- web/app/Models/VisitClient.php
+++ web/app/Models/VisitClient.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class VisitClient extends Model
 {
 
+    protected $table = 'visit_clients';
     protected $guarded = [];
     public function client()
     {
