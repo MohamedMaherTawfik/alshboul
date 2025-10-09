e88b6dc311087ff3bbc91b9eda3aafa39fd2ebf3
Mohamed Maher <m7mdellham77@gmail.com>
2025-10-09 13:56:08 +0300

    ..

--- web/resources/views/admin/mooakl/index.blade.php
+++ web/resources/views/admin/mooakl/index.blade.php
@@ -54,7 +54,7 @@
                             <tbody>
                                 @foreach ($data as $info)
                                     <tr>
-                                        <td>{{ $loop->id ?? 'غير محدد' }}</td>
+                                        <td>{{ $info->id ?? 'غير محدد' }}</td>
                                         <td>{{ $info->name ?? 'غير محدد' }}</td>
                                         <td>{{ $info->national_id ?? 'غير محدد' }}</td>
                                         <td>{{ $info->nationality ?? 'غير محدد' }}</td>
